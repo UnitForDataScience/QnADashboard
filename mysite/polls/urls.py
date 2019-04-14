1950906e862a788368088b1e01f232b5daa609bf
shanky <shashank.prof.05@gmail.com>
2019-04-14 11:03:29 -0700

    Leaving django for good

--- mysite/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('vote/<int:question_id>', views.vote, name='vote'),
-    path('results/<int:question_id>', views.results, name='vote'),
-    path('summary', views.get_summary_keywords, name='vote')
-]
