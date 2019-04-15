22044a67d3321d0feb1a735f440417dce6c79e26
shanky <shashank.prof.05@gmail.com>
2019-04-15 06:04:05 -0700

    base is ready

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, send
+from persistqueue import Queue
+import time, threading
+from threading import Lock
+
+task_queue = Queue('./queue')
 
 app = Flask(__name__)
+socketio = SocketIO(app, async_mode='threading')
+
+
+thread = None
+thread_lock = Lock()
+
+
+def request_thread_reader():
+    while True:
+        if task_queue.qsize():
+            task = task_queue.get()
+            socketio.emit('message', task)
+            task_queue.task_done()
+        time.sleep(1)
+
+
+t1 = threading.Thread(target=request_thread_reader)
+t1.start()
 
 
 @app.route('/')
@@ -8,5 +32,21 @@ def hello_world():
     return render_template('index.html')
 
 
+@socketio.on('connected')
+def connected():
+    print('Someone connected')
+
+
+@socketio.on('request')
+def handle_request(data):
+    data['sid'] = request.sid
+    task_queue.put(data)
+
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Someone disconnected')
+
+
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    socketio.run(app, port=8000, debug=True, host='0.0.0.0')
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,36 @@
 $(document).ready(function () {
-    $('#summarization_table').DataTable({
-        searching: false
+
+    var table = $('#summarization_table').DataTable({
+        searching: false,
+        ordering: false
     });
+    var requests = {};
+    $('#get_summary').on('click', function () {
+        var myFile = $('#summarization_files').prop('files');
+        var files = []
+        for (var i = 0; i < myFile.length; i++) {
+            files.push(myFile[i])
+        }
+        files.forEach(function (file) {
+            var reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+            reader.onload = function (evt) {
+                var row = table.row.add([
+                    file.name,
+                    "Pending....."
+                ]);
+                var uuid = uuidv4();
+                requests[uuid] = row;
+                row.draw()
+                socket.emit('request', {
+                    text: evt.target.result,
+                    request_id: uuid,
+                    type: 'summarize'
+                })
+            };
+            reader.onerror = function (evt) {
+                console.log(evt);
+            }
+        })
+    })
 });
