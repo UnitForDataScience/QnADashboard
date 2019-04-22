9db0f6658b5ae5ef7364b2c0111e0a933c778314
shanky <shashank.prof.05@gmail.com>
2019-04-22 14:19:46 -0700

    zach code

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ def request_thread_reader():
                         task['summary'] = summarize_tf(task['text'],
                                                        keywords=task['keywords'] if 'keywords' in task else [],
                                                        count=task['n'])
+                elif task['type'] == 'QnA':
+                    func(task['text'], task['question'])
+                    task['answer'] = ''
+                    None
                 sid = task['sid']
                 del task['sid']
                 del task['text']
